19d3cfcc200fb6147f38b9f61bfc116fce13de44
Priyanshu <prynsu@yahoo.com>
2021-08-23 02:34:03 +0530

    Fix Under process

--- src/components/pageNotFound.jsx
+++
@@ -1,7 +0,0 @@
-export const PageNotFound = () => {
-    return (
-        <div>
-            Error 404!
-        </div>
-    )
-}
--- src/components/reversePrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Route, Navigate } from 'react-router-dom'
-
-export const ReversePrivateRoute = ({ path, login, ...props }) => {
-  return !login ? (
-    <Route {...props} path={path} />
-  ) : (
-    <Navigate state={{ from: path }} replace to="/" />
-  )
-}
