79f124fde5898673d059bf2a9f3137569d48d938
Priyanshu <prynsu@yahoo.com>
2021-08-23 02:35:18 +0530

    Fix: Removed & Added Components

---
+++ src/components/loginBox.jsx
@@ -0,0 +1,29 @@
+import './styles/utility.css'
+
+export const LoginBox = (props) => {
+  return (
+    <div>
+        <input
+          onChange={props.usernameText}
+          className="inputOne"
+          type="text"
+          placeholder="Username"
+        />
+        <br />
+        <input
+          onChange={props.passwordText}
+          className="inputOne"
+          type="password"
+          placeholder="Password"
+        />
+        <br />
+        <button
+          onClick={props.loginBtnClick}
+          className="btnPrimary btnLogin"
+          type="submit"
+        >
+          Login
+        </button>
+    </div>
+  )
+}
---
+++ src/components/signupBox.jsx
@@ -0,0 +1,39 @@
+import './styles/utility.css'
+
+export const SignupBox = (props) => {
+  return (
+    <div>
+        <input
+          onChange={props.signupName}
+          className="inputOne"
+          type="text"
+          placeholder="Name"
+        />
+        <br />
+        <input
+          onChange={props.signupUsername}
+          className="inputOne"
+          type="text"
+          placeholder="Username"
+        />
+        <br />
+        <input
+          onChange={props.signupEmail}
+          className="inputOne"
+          type="text"
+          placeholder="Email"
+        />
+        <br />
+        <input
+          onChange={props.signupPassword}
+          className="inputOne"
+          type="password"
+          placeholder="Password"
+        />
+        <br />
+        <button onClick={props.signupBtnClick} className="btnPrimary btnLogin">
+          Signup
+        </button>
+    </div>
+  )
+}
