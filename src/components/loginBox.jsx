8622436c0ea1336c2966fcb435ee2c9614a05211
Priyanshu <prynsu@yahoo.com>
2022-05-25 16:55:22 +0530

    Feat: Show Password On/Off

--- src/components/loginBox.jsx
+++ src/components/loginBox.jsx
@@ -1,29 +1,40 @@
 import './styles/utility.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
 
 export const LoginBox = (props) => {
+  const [showPass, setShowPass] = useState(false)
   return (
     <div>
-        <input
-          onChange={props.usernameText}
-          className="inputOne"
-          type="text"
-          placeholder="Username"
-        />
-        <br />
-        <input
-          onChange={props.passwordText}
-          className="inputOne"
-          type="password"
-          placeholder="Password"
-        />
-        <br />
-        <button
-          onClick={props.loginBtnClick}
-          className="btnPrimary btnLogin"
-          type="submit"
-        >
-          Login
-        </button>
+      <input
+        onChange={props.usernameText}
+        className="inputOne"
+        type="text"
+        placeholder="Username"
+      />
+      <br />
+      <input
+        onChange={props.passwordText}
+        className="inputOne"
+        type={showPass ? 'text' : 'password'}
+        placeholder="Password"
+      />
+      <div className='showPasswordContainer'>
+      <input 
+      onChange={()=> setShowPass(showPass => !showPass)}
+      checked={showPass}
+      type='checkbox' />
+      <label>Show Password</label>
+      </div>
+      <br />
+      <button
+        onClick={props.loginBtnClick}
+        className="btnPrimary btnLogin"
+        type="submit"
+      >
+        Login
+      </button>
     </div>
   )
 }
--- src/components/signupBox.jsx
+++ src/components/signupBox.jsx
@@ -1,39 +1,50 @@
 import './styles/utility.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
 
 export const SignupBox = (props) => {
+  const [showPass, setShowPass] = useState(false)
   return (
     <div>
-        <input
-          onChange={props.signupName}
-          className="inputOne"
-          type="text"
-          placeholder="Name"
-        />
-        <br />
-        <input
-          onChange={props.signupUsername}
-          className="inputOne"
-          type="text"
-          placeholder="Username"
-        />
-        <br />
-        <input
-          onChange={props.signupEmail}
-          className="inputOne"
-          type="text"
-          placeholder="Email"
-        />
-        <br />
-        <input
-          onChange={props.signupPassword}
-          className="inputOne"
-          type="password"
-          placeholder="Password"
-        />
-        <br />
-        <button onClick={props.signupBtnClick} className="btnPrimary btnLogin">
-          Signup
-        </button>
+      <input
+        onChange={props.signupName}
+        className="inputOne"
+        type="text"
+        placeholder="Name"
+      />
+      <br />
+      <input
+        onChange={props.signupUsername}
+        className="inputOne"
+        type="text"
+        placeholder="Username"
+      />
+      <br />
+      <input
+        onChange={props.signupEmail}
+        className="inputOne"
+        type="text"
+        placeholder="Email"
+      />
+      <br />
+      <input
+        onChange={props.signupPassword}
+        className="inputOne"
+        type={showPass ? 'text' : 'password'}
+        placeholder="Password"
+      />
+      <div className='showPasswordContainer'>
+      <input 
+      onChange={()=> setShowPass(showPass => !showPass)}
+      checked={showPass}
+      type='checkbox' />
+      <label>Show Password</label>
+      </div>
+      <br />
+      <button onClick={props.signupBtnClick} className="btnPrimary btnLogin">
+        Signup
+      </button>
     </div>
   )
 }
